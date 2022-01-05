6171e931a468b77a71597d4618f7a50377ddf747
cse18107 <cse18107@cemk.ac.in>
2022-01-05 19:30:43 +0530

    working on create routes

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -1,32 +1,14 @@
-const express = require('express');
-
+const express = require("express");
+const placesControllers = require('../controllers/places-controllers');
 const router = express.Router();
 
-const DUMMY_PLACES = [
-    {
-        id:'p1',
-        title:'Empire State Building',
-        description: 'One of the most famous sky scrapers in the world!',
-        location:{
-            lat:40.7484474,
-            lng:-73.987151
-        },
-        address:'20 W 24th building New York, NY 10001',
-        creator:'u1'
-    }
-];
 
-router.get('/:pid',(req,res,next)=>{
-    const placeId = req.params.pid; 
-    const place = DUMMY_PLACES.find(p=>{
-        return p.id===placeId;
-    });
-    res.json({place:place});
-});
+router.get("/:pid",placesControllers.getPlaceById);
 
 
+router.get("/user/:uid",placesControllers.getPlaceByUserId);
 
 
-
+router.post("/",placesControllers.createPlace);
 
 module.exports = router;
